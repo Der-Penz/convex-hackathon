a35d85f603727d9f7176c0e7cc116dc3f4fb59ae
DerPenz <mike2407@t-online.de>
2023-09-06 16:50:39 +0200

    init

---
+++ src/app/Navbar.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+export function Navbar() {
+  return (
+    <div className="border-b py-4">
+      <div className="container mx-auto flex justify-between">
+        <div>LOGO</div>
+        <nav className="flex gap-4">
+        <div>Sign In</div>
+        </nav>
+      </div>
+    </div>
+  );
+}
