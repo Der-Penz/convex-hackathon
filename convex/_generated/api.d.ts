93c77ff840870b8cd80ff4933289113a589552fe
DerPenz <mike2407@t-online.de>
2023-09-14 11:12:02 +0200

    removed old schema

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -14,8 +14,6 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
-import type * as changes from "../changes";
-import type * as documents from "../documents";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -25,10 +23,7 @@ import type * as documents from "../documents";
  * const myFunctionReference = api.myModule.myFunction;
  * ```
  */
-declare const fullApi: ApiFromModules<{
-  changes: typeof changes;
-  documents: typeof documents;
-}>;
+declare const fullApi: ApiFromModules<{}>;
 export declare const api: FilterApi<
   typeof fullApi,
   FunctionReference<any, "public">
--- convex/schema.ts
+++ convex/schema.ts
@@ -1,15 +1,4 @@
 import { defineSchema, defineTable } from 'convex/server';
 import { v } from 'convex/values';
 
-export default defineSchema({
-	document: defineTable({
-		creator: v.string(),
-		filetype: v.string(),
-		name: v.string(),
-	}),
-	change: defineTable({
-		editor: v.string(),
-		change: v.any(),
-		documentId: v.id('document'),
-	}),
-});
+export default defineSchema({});
